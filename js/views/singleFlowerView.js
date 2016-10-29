e05c9d1025dbd1da76266af4eed9678dd9d5a5ec
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2016-10-29 17:35:02 -0200

    Created and configured the template to render the data.

--- js/views/singleFlowerView.js
+++ js/views/singleFlowerView.js
@@ -2,6 +2,14 @@ var app = app || {};
 
 app.singleFlowerView=Backbone.View.extend({
 tagName: 'article',
-className: 'flowerListItem' 
+className: 'flowerListItem',
+template : _.template($('#flowerElement').html()),
+render: function(){
+var flowerTemplate = this.template(this.model.toJSON());
+this.$el.html(flowerTemplate);
+return this;
+
+}
+
 
 });
