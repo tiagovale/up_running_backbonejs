25d2120fba0493146b0b7b018a3499a5188cb7a7
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2016-10-29 18:20:09 -0200

    Configured the basic structure for routing.

---
+++ js/routes/router.js
@@ -0,0 +1,14 @@
+var app = app || {};
+
+app.Router = Backbone.Router.extend({
+
+routes :{
+  "": "noCopy",
+  "heirloomRose" : "heirloomRoseMessage",
+  "rainbowRose": "rainbowRoseMessage",
+  "redRose" : "redRoseMessage"
+},
+
+
+
+});
